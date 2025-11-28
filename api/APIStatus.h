8d6e0b0c76ac0c97fc785aa685ea14141f822f7b
passplease <fkyfeweiruanyouxiang@outlook.com>
2025-11-28 13:52:48 +0800

    Init Master

--- api/APIStatus.h
+++
@@ -1,5 +0,0 @@
-#ifdef API_DLL
-#define API __declspec(dllexport)
-#else
-#define API __declspec(dllimport)
-#endif
--- api/pluginInterface.cpp
+++
@@ -1,6 +0,0 @@
-#include "pluginInterface.h"
-
-Task::Task(const char *keyword, WorkingMode mode) {
-    this -> keyword = keyword;
-    this -> mode = mode;
-}
